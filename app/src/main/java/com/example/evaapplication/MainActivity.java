04bc74b8f2af35b79578140963f9ebfefe8a0ef2
lina <lina.maudlej@gmail.com>
2020-10-06 10:35:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/evaapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.evaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
